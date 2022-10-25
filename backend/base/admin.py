e2a3150a22bf9933bdf4ede7021f01a11110281a
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-26 00:05:47 +0330

    Registered the models in admin panel

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from base.models import Course , Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['contractNo', 'create', 'update', 'isComplete',]
+    list_filter = ['create', 'isComplete',]
+    search_fields = ['name',]
+    date_hierarchy = ('create')
+    
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['course', 'lessonNo', 'create', 'update', 'isRecord','isConfirm',]
+    list_filter = ['create', 'isRecord', 'isConfirm',]
+    search_fields = ['name',]
+    date_hierarchy = ('create')
+    

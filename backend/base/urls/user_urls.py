b4b2ff2294032e6feb6b7b9906fad30bcc380f1f
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-25 23:11:19 +0330

    registerUser OK

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('login/', views.MyCreatorTokenView.as_view(), name='login'),
     path('refresh/token/', TokenRefreshView.as_view(), name='refresh_token'),  
+    path('register/', views.registerUser, name='user-register'),
     
     path('profile/', views.userProfile, name='user-profile')
 ] 
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 
 from rest_framework import status 
 from rest_framework.response import Response 
@@ -9,6 +10,8 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from base.serializers import UserSerializer
+from base.strConst import *
+from base.utils.email_utils import activation_email
 
 class MyCreatorTokenSerializer(TokenObtainPairSerializer):
 
@@ -42,4 +45,36 @@ def userProfile(request):
         user.save()
         return Response(UserSerializer(user,many=False).data)
     
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    username = data['email']
+    password = data['password']
     
+    try:
+        user = User.objects.create(
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password']),
+            first_name = data['name'],
+            is_active=False
+        )
+        if(activation_email(user)):
+            return Response({DETAIL: WELCOME_TO_MCM}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({DETAIL: ERR_SENT_EMAIL}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    except Exception as e:
+        print(e)
+        # کاربری با ایمیل مشابه در دیتابیس هست. حالا یا فعال نیست یا گذرواژه خودرا فراموش کرده
+        user = User.objects.get(username=username)
+        user.password = make_password(password)
+        if user and not user.is_active:
+            if(activation_email(user)):
+                return Response({DETAIL:ACCOUNT_IS_EX_BUT_NOT_ACTIVE}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({DETAIL: ERR_SENT_EMAIL}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif user and user.is_active:
+            return Response({DETAIL: PLEASE_LOGIN}, status=status.HTTP_400_BAD_REQUEST)
+           
+       

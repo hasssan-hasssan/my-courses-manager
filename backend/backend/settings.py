8d0d2c1daf3d4413be81d84b37a997bac99e4597
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-25 20:51:09 +0330

    Installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,12 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    # Third party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
+    # My apps
+    'base.apps.BaseConfig',
+    
+    
 ]
 
 MIDDLEWARE = [

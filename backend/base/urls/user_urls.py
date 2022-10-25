14a62001613632b90140d55da63388be6493420b
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-25 21:37:37 +0330

    Refresh token OK

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from base.views import user_views as views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', views.MyCreatorTokenView.as_view(), name='login'),
-]
+    path('refresh/token/', TokenRefreshView.as_view(), name='refresh_token'),  
+] 

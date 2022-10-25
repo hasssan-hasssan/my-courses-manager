4d5013486420c6a1b76e4729367dbd398fb480e3
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-25 21:44:21 +0330

    See user profile is OK

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,4 +5,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('login/', views.MyCreatorTokenView.as_view(), name='login'),
     path('refresh/token/', TokenRefreshView.as_view(), name='refresh_token'),  
+    
+    path('profile/', views.userProfile, name='user-profile')
 ] 
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,3 +1,8 @@
+from rest_framework import status 
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -18,3 +23,14 @@ class MyCreatorTokenSerializer(TokenObtainPairSerializer):
 
 class MyCreatorTokenView(TokenObtainPairView):
     serializer_class = MyCreatorTokenSerializer
+    
+   
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def userProfile(request):
+    
+    if request.method == 'GET':
+        user = request.user
+        return Response(UserSerializer(user,many=False).data)
+    
+    

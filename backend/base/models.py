aee44b2184cec41b73b6fc95d58545ba22e7de0a
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-25 23:51:15 +0330

    Course and Lesson Model added to base.models

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    contractNo = models.CharField(max_length=100)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    isComplete = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.contractNo
+    
+
+class Lesson(models.Model):
+    lessonNo = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    isRecord = models.BooleanField(default=False)
+    isConfirm =models.BooleanField(default=False) 
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{str(self.course.contractNo)} / L-{self.lessonNo}"
+    

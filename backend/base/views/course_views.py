4f5e0a3377dd694387cfc8036b33a24f4508a7aa
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-26 14:56:19 +0330

    getMyCourse PUT OK:

--- backend/base/views/course_views.py
+++ backend/base/views/course_views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from base.models import Course
 from base.serializers import CourseSerializer
 from base.strConst import *
+from base.utils.condition_utils import areLessonsRecordAndConfirm
 
 
 @api_view(['POST', 'GET'])
@@ -34,18 +35,35 @@ def getMyCourses(request):
             return Response({DETAIL:COURSE_CREATE_FAIL}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
 def getMyCourse(request, contNo):
     user = request.user
-    
-    if request.method == 'GET':
-        try:
-            course = user.course_set.get(contractNo=contNo)
-        except Course.DoesNotExist:
-            return Response({DETAIL:COURSE_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
-        else:
+        
+    try:
+        course = user.course_set.get(contractNo=contNo)
+    except Course.DoesNotExist:
+        return Response({DETAIL:COURSE_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        if request.method == 'GET':
             return Response(CourseSerializer(course,many=False).data)
-    
+        elif request.method == 'PUT':
+            data = request.data
+            course.name = data['name']
+            if data['isComplete'] == 'True':
+                # هر دوره برای تکمیل شدن باید حداقل یک درس تایید شده و ضبط شده داشته باشد
+                if len(course.lesson_set.all()) == 0:
+                    return Response({DETAIL: COURSE_RULES_1}, status=status.HTTP_400_BAD_REQUEST)
+                elif (areLessonsRecordAndConfirm(course)): # آیا همه ی درس ها ضبط و تایید شده
+                    course.isComplete = data['isComplete']
+                    course.save()
+                    return Response(CourseSerializer(course, many=False).data)
+                else:
+                    return Response({DETAIL: COURSE_LESSONS_ALERT_1}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                course.isComplete = data['isComplete']
+                course.save()
+                return Response(CourseSerializer(course, many=False).data)
+
     
         

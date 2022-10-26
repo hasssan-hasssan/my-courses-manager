0cef379bf6554858dd573b0ce5fc3f139353ac26
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-10-26 17:51:58 +0330

    getMyCourseLessons GET OK

--- backend/base/urls/course_urls.py
+++ backend/base/urls/course_urls.py
@@ -4,4 +4,5 @@ from base.views import course_views as views
 urlpatterns = [
     path('my/', views.getMyCourses, name='users-courses'),
     path('<str:contNo>/', views.getMyCourse, name='user-course'),
+    path('<str:contNo>/l-all/', views.getMyCourseLessons, name='user-courser-lessons')
 ]
--- backend/base/views/course_views.py
+++ backend/base/views/course_views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-from base.models import Course
-from base.serializers import CourseSerializer
+from base.models import Course, Lesson
+from base.serializers import CourseSerializer, LessonSerializer
 from base.strConst import *
 from base.utils.condition_utils import areLessonsRecordAndConfirm
 
@@ -17,7 +17,7 @@ def getMyCourses(request):
     if request.method == 'GET':
         try:
             courses = user.course_set.all()
-        except Course.DoesNotExist:
+        except:
             return Response({DETAIL: COURSES_NOT_EX_YET}, status=status.HTTP_404_NOT_FOUND)
         else:
             return Response(CourseSerializer(courses, many=True).data)
@@ -71,4 +71,17 @@ def getMyCourse(request, contNo):
             except:
                 return Response({DETAIL: COURSE_DEL_FAIL}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-        
+ 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyCourseLessons(request, contNo):
+    user = request.user
+    
+    try:
+        course = user.course_set.get(contractNo=contNo)
+    except Course.DoesNotExist:
+        return Response({DETAIL: COURSE_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
+    else:
+        if request.method == 'GET':
+            lessons = course.lesson_set.all().order_by('lessonNo')
+            return Response(LessonSerializer(lessons,many=True).data)       
